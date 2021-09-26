f7bab9471d8b9a591a888c805fbcd505bbdb4dca
SimonBr017 <simonbrard017@gmail.com>
2021-09-26 11:23:32 +0200

    semistandart

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,8 +18,8 @@ $(document).ready(function () {
 $(function () {
   const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, function (data) {
-    if (data.status === 'OK') {
+	  if (data.status === 'OK') {
       $('header #api_status').addClass('available');
-    }
+	  }
   });
 });
